0c7d7ed5705088016b488fac50d03554abf99e66
muthaiahpanchanathan <muthaiah.p2023csbs@sece.ac.in>
2026-03-16 18:15:42 +0530

    added seatprice fixation

--- controllers/seatController.js
+++ controllers/seatController.js
@@ -13,7 +13,7 @@ const addSeat=asyncHandler(async(req,res)=>{
     if(typeof(seatCount)!="number" || seatCount<=0)
         throw new AppError(400,"Invalid seat count")
 
-    const [existingScreen]=await pool.query("select * from screens where id=?",screenId)
+    const [existingScreen]=await pool.query("select * from screens where id=?",[screenId])
     if(existingScreen.length==0)
         throw new AppError(404,"screen not found")
 
@@ -40,9 +40,9 @@ const addSeat=asyncHandler(async(req,res)=>{
     const arr=[];
     for(let seat of rows){
         for(let i=1;i<=seatCount;i++){
-            if(premium.includes(seat))
+            if(premiumSet.has(seat))
                 arr.push([screenId,seat,i,"premium"])
-            else if(lounge.includes(seat))
+            else if(loungeSet.has(seat))
                 arr.push([screenId,seat,i,"lounge"])
             else
                 arr.push([screenId,seat,i,"normal"])
@@ -56,7 +56,7 @@ const getSeats=asyncHandler(async(req,res)=>{ //show prices after adding showPri
     const screenId=req.query.screenId
     if(!screenId)
         throw new AppError(400,"Screen Id cannot be empty")
-    const [existingScreen]=await pool.query("select * from screens where id=?",screenId)
+    const [existingScreen]=await pool.query("select * from screens where id=?",[screenId])
     if(existingScreen.length==0)
         throw new AppError(404,"screen doesnt exist")
     const [seats]=await pool.query("select * from seats where screenId=?",[screenId])
@@ -67,18 +67,66 @@ const deleteSeats=asyncHandler(async(req,res)=>{
     const screenId=req.query.screenId
     if(!screenId)
         throw new AppError(400,"Screen Id cannot be empty")
-    const [existingSeating]=await pool.query("select * from seats where screenId=?",screenId)
+    const [existingSeating]=await pool.query("select * from seats where screenId=?",[screenId])
     if(existingSeating.length==0)
         throw new AppError(404,"seating arrangement doesnt exist for this screen")
-    const [existingScreen]=await pool.query("select * from screens where id=?",screenId)
+    const [existingScreen]=await pool.query("select * from screens where id=?",[screenId])
     if(existingScreen.length==0)
         throw new AppError(404,"screen doesnt exist")
     const currDate=new Date();
-    const [existingShows]=await pool.query("select * from shows where ScreenId=? and showDate>=?",[screenId,currDate])
+    const [existingShows]=await pool.query("select * from shows where screenId=? and showDate>=?",[screenId,currDate])
     if(existingShows.length>0)
         throw new AppError(409,"Cannot delete seats because shows are already scheduled for this screen")
     const [result]=await pool.query("delete from seats where screenId=?",[screenId])
     return res.status(200).json({message:"seats deleted successfully"})
 })
 
-module.exports={addSeat,getSeats,deleteSeats}
+const setSeatPrices=asyncHandler(async(req,res)=>{
+    const showId=parseInt(req.query.showId)
+    if(!showId)
+        throw new AppError(400,"Show Id cannot be empty")
+    let {normalPrice,premiumPrice,loungePrice}=req.body
+    if(!normalPrice || !premiumPrice || !loungePrice)
+        throw new AppError(400,"All price fields are required")
+    normalPrice=Number(normalPrice)
+    premiumPrice=Number(premiumPrice)
+    loungePrice=Number(loungePrice)
+    const [showDetails]=await pool.query("select screenId from shows where id=?",[showId])
+    if(showDetails.length==0)
+        throw new AppError(404,"Show not found")
+    const [existingScreen]=await pool.query("select id from screens where id=?",[showDetails[0].screenId])
+    if(existingScreen.length==0)
+        throw new AppError(404,"Show Screen doesnt exist")
+    const [existingSeats]=await pool.query("select id from seats where screenId=?",[existingScreen[0].id])
+    if(existingSeats.length==0)
+        throw new AppError(404,"seating arrangement doesnt exist for this screen")
+
+    const [existingPricing]=await pool.query("select * from showPrice where showId=?",[showId])
+    if(existingPricing.length>0){
+        await pool.query(`update showPrice 
+                          set price= CASE 
+                              when seatType="normal" then  ?
+                              when seatType="premium" then ?
+                              when seatType="lounge" then ?
+                          END
+                          where showId=?`,[normalPrice,premiumPrice,loungePrice,showId])
+        return res.status(200).json({message:"Seat prices updated successfully"})
+    }
+    else{
+        await pool.query(`insert into showPrice (seatType,price,showId) 
+                          values 
+                          ('normal',?,?),
+                          ('premium',?,?),
+                          ('lounge',?,?)`,
+                        [
+                            normalPrice,showId,
+                            premiumPrice,showId,
+                            loungePrice,showId
+                        ])
+        return res.status(200).json({message:"Seat prices set successfully"})
+    }
+})
+
+
+
+module.exports={addSeat,getSeats,deleteSeats,setSeatPrices}
--- routes/seatRouter.js
+++ routes/seatRouter.js
@@ -1,9 +1,10 @@
 const express=require("express")
-const {addSeat, getSeats,deleteSeats}=require("../controllers/seatController")
+const {addSeat, getSeats,deleteSeats, setSeatPrices}=require("../controllers/seatController")
 const router=express.Router();
 
 router.post("/",addSeat)
 router.get("/",getSeats)
 router.delete("/",deleteSeats)
+router.post("/price",setSeatPrices)
 
 module.exports=router
