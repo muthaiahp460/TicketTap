7344a29fc0060aaf8aba5625f251c2271761334e
muthaiahpanchanathan <muthaiah.p2023csbs@sece.ac.in>
2026-03-19 23:18:07 +0530

    Added refund for timedout transactions

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -31,9 +31,7 @@ const bookTickets=asyncHandler(async(req,res,next)=>{
              inner join showPrice on showSeats.showId=showPrice.showId and seats.type=showPrice.seatType
              where showSeats.id in (${seatIds})`)
         const total=totalPrice[0].price;
-        console.log(req.user)
         const booking=await connection.query("insert into bookings (userId,showId,totalAmount,status) values (?,?,?,?)",[req.user.id,showId,total,"pending"])
-        console.log(booking)
         const data=[]
         for(seat of seatIds){
             data.push([booking[0].insertId,seat,showId])
@@ -59,20 +57,23 @@ const payment=asyncHandler(async(req,res,next)=>{
         const currTime=getCurrTime();
         const [show]=await connection.query("select showId from bookings where id=?",[bookingId])
         const showId=show[0].showId
-        const [bookedSeats]=await pool.query("select showSeats.seatId from bookingSeat inner join showSeats on bookingSeat.seatId=showSeats.seatId and bookingSeat.showId=showSeats.showId where bookingSeat.bookingId=?",bookingId)
+        const [bookedSeats]=await connection.query("select showSeats.seatId from bookingSeat inner join showSeats on bookingSeat.seatId=showSeats.seatId and bookingSeat.showId=showSeats.showId where bookingSeat.bookingId=?",bookingId)
         
         const seatsBooked=[]
         for(seat of bookedSeats)
             seatsBooked.push(seat.seatId)
 
         const seatId=bookedSeats[0].seatId
-        const [lockedSeats]=await pool.query(`select id from showSeats where seatId=? and status=? and expiresAt<? and showId=?`,[seatId,"pending",currTime,showId])
+        const [lockedSeats]=await connection.query(`select id from showSeats where seatId=? and status=? and expiresAt<? and showId=?`,[seatId,"pending",currTime,showId])
 
-        if(lockedSeats.length==0)
+        if(lockedSeats.length==0){
+            await connection.query("insert into refund (bookingId) values (?)",[bookingId])
+            await connection.commit()
             throw new AppError(410,"Seat released payment will be refunded within 2-3 working days")
+        }
 
-        await pool.query(`update showSeats set status=? where seatId in (${seatsBooked})`,["booked"])
-        await pool.query("update bookings set status=? where id=?",["completed",bookingId])
+        await connection.query(`update showSeats set status=? where seatId in (${seatsBooked})`,["booked"])
+        await connection.query("update bookings set status=? where id=?",["completed",bookingId])
         await connection.commit()
 
         return res.status(200).json({message:"booking sucessful"})
