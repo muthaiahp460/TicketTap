add56bb2fb372e7aa80993c85a017030f7c2c062
muthaiahpanchanathan <muthaiah.p2023csbs@sece.ac.in>
2026-05-21 11:46:00 +0530

    fix2:cookie issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,7 +173,12 @@ const verifytoken=(req,res)=>{
 }
 
 const logout=(req,res)=>{
-    res.clearCookie("token");
+    res.clearCookie("token",{
+        httpOnly:true,
+        secure:true,
+        sameSite:"None"
+    });
+
     res.status(200).json({success:true})
 }
 
